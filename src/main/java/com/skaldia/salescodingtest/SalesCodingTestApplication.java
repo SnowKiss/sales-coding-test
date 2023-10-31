15dd357751591fff81a0db7716a6fe91527c2fab
SnowKiss <johnschmitt9@gmail.com>
2023-10-31 12:03:15 +0100

    Initial commit

---
+++ src/main/java/com/skaldia/salescodingtest/SalesCodingTestApplication.java
@@ -0,0 +1,13 @@
+package com.skaldia.salescodingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesCodingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesCodingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skaldia/salescodingtest/SalesCodingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skaldia.salescodingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesCodingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
